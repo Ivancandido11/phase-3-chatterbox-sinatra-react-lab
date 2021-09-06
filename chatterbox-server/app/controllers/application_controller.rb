795a73dbf0d5303eb27b676f07f3b04b8354b73f
Ivan Candido <ivan.candido@powerhrg.com>
2021-09-06 19:49:59 -0400

    Completed Assignment

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,32 @@
 class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
-  
-  # Add routes
-  
+  set :default_content_type, "application/json"
+  get "/messages" do
+    messages = Message.all
+    messages.to_json
+  end
+
+  post "/messages" do
+    message = Message.create(
+      body: params[:body],
+      username: params[:username],
+      created_at: params[:created_at],
+      updated_at: nil
+    )
+    message.to_json
+  end
+
+  patch "/messages/:id" do
+    message = Message.find(params[:id])
+    message.update(
+      body: params[:body],
+      updated_at: params[:updated_at]
+    )
+    message.to_json
+  end
+
+  delete "/messages/:id" do
+    message = Message.find(params[:id])
+    message.destroy
+    message.to_json
+  end
 end
